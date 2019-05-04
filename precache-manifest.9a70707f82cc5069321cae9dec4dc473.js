616126418b57564ad2c29e1d14135734684fb9a0
Koaul <ohapckin.andrej@yandex.ru>
2019-05-04 22:38:29 +0300

    Updates

---
+++ precache-manifest.9a70707f82cc5069321cae9dec4dc473.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3622793a0ad4803229ce6c4799f499",
+    "url": "/movie-searcher/index.html"
+  },
+  {
+    "revision": "ad73aa70d3111c9fbcc1",
+    "url": "/movie-searcher/static/js/2.b6a07bd9.chunk.js"
+  },
+  {
+    "revision": "7ed371e5185723b23781",
+    "url": "/movie-searcher/static/js/main.3caea85c.chunk.js"
+  },
+  {
+    "revision": "f23735500ca762c1228d",
+    "url": "/movie-searcher/static/js/runtime~main.ed79a067.js"
+  }
+]);
